f46712483077849554134503fadbe3f7b085408c
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-03-10 10:32:18 -0500

    added dev package

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button as MaterialButton } from '@mui/material'
+
+type Props = {}
+
+export default function Button({}: Props) {
+  return (
+    <MaterialButton variant='outlined'>Button</MaterialButton>
+  )
+}
